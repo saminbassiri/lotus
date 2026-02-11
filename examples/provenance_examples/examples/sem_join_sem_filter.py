0cbf017b364eb89e82528824650f92cc86b248c8
fridtjof damm <soenke.f.damm@campus.tu-berlin.de>
2026-02-11 04:55:59 +0100

    Add examples

---
+++ examples/provenance_examples/examples/sem_join_sem_filter.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import lotus
+from lotus.models import LM
+
+from data.sqllite_db import get_data_from_sql_as_dict, DB_PATH
+
+lm = LM(model=f"ollama/llama3.2:3b")
+
+lotus.settings.configure(lm=lm)
+
+
+def join_filter_movie_reviews(df, use_prov=False):
+
+    provenance_col = "provenance_id" if use_prov else None
+
+    extracted_df = df.sem_extract(
+        ["review"],
+        {"key_quote": "A short characteristic quote from the review."},
+        provenance=use_prov,
+        provenance_col=provenance_col,
+    )
+
+    filtered_df = extracted_df.sem_filter(
+        "{review} expresses a negative sentiment toward the film", provenance=use_prov, provenance_col=provenance_col
+    )
+    categories = pd.DataFrame(
+        {"category": ["technical and analytical", "emotional and subjective"]}
+    )
+    joined_df = filtered_df.sem_join(
+        categories,
+        "{review} primarily falls under the {category} style of writing. Only answer with the EXACT category.",
+        provenance=use_prov,
+        provenance_col=provenance_col,
+    )
+    bench_df = joined_df.sem_filter(
+        "{key_quote} fits the {category} style",
+        provenance=use_prov,
+        provenance_col=provenance_col,
+    )
+    
+    return bench_df
+
+
+if __name__ == "__main__":
+
+    data = get_data_from_sql_as_dict(DB_PATH, limit=5)
+
+    df = pd.DataFrame(data)
+    print(join_filter_movie_reviews(df, use_prov=True))
---
+++ examples/provenance_examples/examples/sem_topk_sem_map.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import lotus
+from lotus.models import LM
+
+from data.sqllite_db import get_data_from_sql_as_dict, DB_PATH
+
+lm = LM(model=f"ollama/llama3.2:3b")
+
+lotus.settings.configure(lm=lm)
+
+def topk_map_movie_reviews(df, use_prov=False):
+    
+    provenance_col = "provenance_id" if use_prov else None
+    
+    top_reviews = df.sem_topk(
+        "Which {review} shows the most extreme positive enthusiasm?",
+        K=5,
+        provenance=use_prov,
+        provenance_col=provenance_col,
+    )
+    reasoned_reviews = top_reviews.sem_map(
+        "Given the {review}, list the top 3 adjectives that express the user's joy. Output: [adj1, adj2, adj3]",
+        suffix="Extracted_Keywords",
+        provenance=use_prov,
+        provenance_col=provenance_col,
+    )
+    aggregated_df = reasoned_reviews.sem_agg(
+        "Summarize the vocabulary of joy found in these {Extracted_Keywords}?",
+        provenance=use_prov,
+        provenance_col=provenance_col,
+    )
+    target_col = aggregated_df.columns[0]
+    audiences = pd.DataFrame(
+        {"target_audience": ["Cinephiles", "Mainstream Popcorn Fans"]}
+    )
+    bench_df = aggregated_df.sem_join(
+        audiences,
+        f"Would a {{target_audience:right}} member use the keywords {{{target_col}:left}}?",
+        provenance=use_prov,
+        provenance_col=provenance_col,
+    )
+    
+    return bench_df
+
+if __name__ == "__main__":
+    
+    data = get_data_from_sql_as_dict(DB_PATH, limit=5)
+
+    df = pd.DataFrame(data)
+    print(topk_map_movie_reviews(df, use_prov=True))
